15dc8ba655e6661230542abbb628850728c1dbf2
Josefine Klintberg <josefine.klintberg@hotmail.com>
2019-06-27 14:29:02 +0200

    Voronoi pattern
    A start of the implementation of procedural Voronoi pattern. Start of building a template that will enabling user input to send uniform variables to the fragment shader.

---
+++ Voronoi/fragment.glsl
@@ -0,0 +1,70 @@
+# version 330 core
+
+//Global variables
+uniform float time;
+uniform int colorMode = 0;
+uniform int numb = 200;
+uniform float enhanceFactor = 5;
+uniform float speed;
+uniform vec2 resolution;
+
+in vec3 normals;
+in vec3 position;
+out vec4 FragColor;  
+
+//Generate 2 somewhat random numbers
+vec2 random2( vec2 p, float i ) {
+    return fract(sin(vec2(dot(p,vec2(15.2,i)),dot(p,vec2(26,18))))*4785.3);
+}
+
+//Generate pseudo random numbers, following "The book of Shaders"
+float random(vec2 st) {
+    return fract(sin(dot(st.xy,vec2(12.9898,78.233)))*43758.5453123);
+}
+
+void main()
+{
+
+	//Get the coord. position on the window
+    vec2 thePosition = gl_FragCoord.xy/resolution.xy;
+
+	//Set the backgroundcolor
+	vec3 color = vec3(0.2);
+
+    // Store the points in a vector
+    vec2 point[200];
+    float minDist = 1;  
+
+    // Iterate through the points positions
+    for (int i = 0; i < 200; i++) {
+		
+		//If moving
+		//point[i] = random2(vec2(float(i),float(i)), sin(time/2000000));
+
+		//If not moving
+		point[i] = random2(vec2(float(i), float(i)), i*i/323132);
+		
+		//Calculate the distance between the coordinate and the current point
+        float dist = distance(thePosition, point[i]);
+
+        //Update the minimum distance only if the new distance is smaller
+        minDist = min(minDist, dist);
+    }
+
+    //Highlight the color for the minimum distance
+    color += minDist*enhanceFactor;
+
+	//Set the output color
+    FragColor = vec4(color,1.0);
+}
+
+
+
+
+
+
+
+//Thoughts
+//Want to be able to locate a preset number of points
+//Send in uniform variables by using a menu interface
+//Extend by the possibility to add roughness to the surface??
---
+++ Voronoi/vertex.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+
+layout (location = 0) in vec3 inPos;   // the position variable has attribute position 0
+layout (location = 1) in vec3 inNorm; // the normal variable has attribute position 1
+  
+out vec3 normals;
+out vec3 position;
+
+void main(){
+
+	//Set the position for the vertices
+    gl_Position = vec4(inPos, 1.0);
+
+    //Send to fragment shader
+	position = inPos;
+    normals = inNorm; 
+}  
