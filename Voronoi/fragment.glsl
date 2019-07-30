0e780fccfd3a75d40114e0174b6f24413e9690e3
Josefine Klintberg <josefine.klintberg@hotmail.com>
2019-07-30 12:56:04 +0200

    Lightning added
    Phong local reflection model added to the Voronoi

--- Voronoi/fragment.glsl
+++ Voronoi/fragment.glsl
@@ -12,6 +12,14 @@ in vec3 normals;
 in vec3 position;
 out vec4 FragColor;  
 
+//Lightning model according to the Phong lighting model
+float ambientStrength = 0.8;
+float specularStrength = 1;
+
+vec3 objectColor = vec3(0.9f, 0.9f, 0.9f);
+vec3 lightColor = vec3(0.9f, 0.9f, 0.9f);
+vec3 lightDirection = vec3(-0.2, -0.1, 1);
+
 //Generate 2 somewhat random numbers
 vec2 random2( vec2 p, float i ) {
     return fract(sin(vec2(dot(p,vec2(15.2,i)),dot(p,vec2(26,18))))*4785.3);
@@ -24,7 +32,26 @@ float random(vec2 st) {
 
 void main()
 {
-	const int SEEDS = 500;
+    //Light model
+    
+    //Ambient lightning
+	vec3 Ia = ambientStrength*lightColor;
+
+	//Diffuse color
+	vec3 norm = normalize(normals);
+	vec3 lightDir = normalize(lightDirection - position);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 Id = diff*lightColor; //Light model
+
+    //Specular lighting
+	vec3 viewDir = normalize(lightDirection - position);
+	vec3 reflectDir = max(reflect(-lightDir, norm), 0.0);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 4);
+	vec3 Is = 10*specularStrength * spec * lightColor;
+
+
+    //Voronoi pattern
+	const int SEEDS = 600;
 
 	//Get the coord. position on the window
     vec2 thePosition = gl_FragCoord.xy/resolution.xy;
@@ -40,7 +67,7 @@ void main()
     for (int i = 0; i < SEEDS; i++) {
 		
 		//If moving
-		//point[i] = random2(vec2(float(i),float(i)), sin(time/2000000));
+		//point[i] = random2(vec2(float(i),float(i)), sin(time/20000));
 
 		//If not moving
 		point[i] = random2(vec2(float(i), float(i)), i*i/323132);
@@ -56,10 +83,10 @@ void main()
     color += minDist*enhanceFactor;
 
 	//Display the seeds
-	color += (1.-step(.002, minDist))*0.3;
+	//color += (1.-step(.002, minDist))*0.7;
 
 	//Set the output color
-    FragColor = vec4(color,1.0);
+    FragColor = (1.5*vec4(Ia, 1.0) + 10*vec4(Id, 1.0) + 0.7*vec4(Is, 1.0))*vec4(objectColor, 1.0)*vec4(color, 1.0);
 }
 
 
@@ -72,3 +99,4 @@ void main()
 //Want to be able to locate a preset number of points
 //Send in uniform variables by using a menu interface
 //Extend by the possibility to add roughness to the surface??
+
